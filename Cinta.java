32d4106731dd35fb1b7816a1d3a95e772130c6ad
Erick-XMB <erick.martinezkmn@ciencias.unam.mx>
2026-02-17 10:55:42 -0600

    agregando metodos de cinta menos color

--- Cinta.java
+++ Cinta.java
@@ -8,59 +8,40 @@
  * 
  */
 
-public class Cinta{
+public class Cinta {
 
 	/** Nombre de identificacion de la cinta */
 	private String nombre;
 
-	/** Color de la cinta */
-	private String color;
-
-
 	/**
-     * Constructor de la clase {@code Cinta}
-     * 
-     * @param nombre nombre de la cinta
-     * @param color color de la cinta
-     */
-	public Cinta(String nombre, String color){
+	 * Constructor de la clase {@code Cinta}
+	 * 
+	 * @param nombre nombre de la cinta
+	 */
+	public Cinta(String nombre) {
 		this.nombre = nombre;
-		this.color = color;
 	}
 
 	/** Getters */
 
 	/**
 	 * Metodo que obtiene el nombre de la cinta
+	 * 
 	 * @return Nombre de la cinta
 	 */
-	public String getNombre(){
+	public String getNombre() {
 		return nombre;
-	}
-
-	/**
-	 * Metodo que obtiene el color de la cinta
-	 * @return Color de la cinta
-	 */
-	public String getColor(){
-		return color;
 	}
 
 	/** Setters */
 
 	/**
 	 * Metodo que asigna el nombre a la cinta
+	 * 
 	 * @param nombre nombre a asignar
 	 */
-	public void setNombre(String nombre){
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	/**
-	 * Metodo que asigna el color a la cinta
-	 * @param color color a asignar
-	 */
-	public void setColor(String color){
-		this.color = color;
-	}
 }
